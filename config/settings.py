e45b512e557ae84790fd2cabe4181a859bc18d09
ssignara <ssignara@gmail.com>
2026-07-09 15:32:29 +0200

    feat: configure project settings for Kestra execution

--- config/settings.py
+++ config/settings.py
@@ -4,14 +4,14 @@ import os
 load_dotenv()
 
 # PostgreSQL
-POSTGRES_HOST = os.getenv("DB_HOST")
-POSTGRES_PORT = os.getenv("DB_PORT")
-POSTGRES_DATABASE = os.getenv("DB_NAME")
-POSTGRES_USER = os.getenv("DB_USER")
-POSTGRES_PASSWORD = os.getenv("DB_PASSWORD")
+POSTGRES_HOST = os.getenv("DB_HOST", "localhost")
+POSTGRES_PORT = os.getenv("DB_PORT", "5432")
+POSTGRES_DATABASE = os.getenv("DB_NAME", "sport_data")
+POSTGRES_USER = os.getenv("DB_USER", "sport_user")
+POSTGRES_PASSWORD = os.getenv("DB_PASSWORD", "sport_password")
 
 # Redpanda / Kafka
-KAFKA_BOOTSTRAP_SERVER = "localhost:9092"
+KAFKA_BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER", "localhost:9092")
 
 # Slack
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
